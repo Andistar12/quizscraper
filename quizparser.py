a115a864fa56f083fa93b29742b6166d36b57af2
andy <nguyenandy41@gmail.com>
2017-10-18 16:56:00 -0700

    Add to repository

---
+++ quizparser.py
@@ -0,0 +1,27 @@
+import json
+import urllib.request
+
+url = "https://apis.getmindmap.com/api/story/quizResponse/5170A"
+
+def downloadJSON(link):
+	"""Downloads and parses a JSON from the specified link"""
+	with urllib.request.urlopen(link) as url:
+		data = url.read().decode()
+		return json.loads(data)
+
+json = downloadJSON(url)
+
+#Quiz Data
+quizID = json["_id"]
+quizResponses = [] # [quiz_id, response, id, date]
+
+#Quiz Responses
+for quiz in json["quiz_response"]:
+	quizResponses.append( [quiz["quiz_id"], quiz["response"], quiz["_id"], quiz["date"]] )
+
+#Save info
+with open(quizID + ".csv", "w") as file:
+	for quiz in quizResponses:
+		file.write("{0},{1},{2},{3}\n".format(quiz[0], quiz[1], quiz[2], quiz[3]))
+
+print("Finished processing {0} quizzes".format(len(quizResponses)))
