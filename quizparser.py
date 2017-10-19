668b58ebff52d9885b75fea80248f18d7f8cc672
andy <nguyenandy41@gmail.com>
2017-10-18 22:28:00 -0700

    Recode quiz_id

--- quizparser.py
+++ quizparser.py
@@ -19,9 +19,15 @@ quizResponses = [] # [quiz_id, response, id, date]
 for quiz in json["quiz_response"]:
 	quizResponses.append( [quiz["quiz_id"], quiz["response"], quiz["_id"], quiz["date"]] )
 
+#Sort responses
+quizResponses = sorted(quizResponses, key = lambda x: x[0])
+for count, quiz in enumerate(quizResponses, 1):
+	quiz[0] = count
+
 #Save info
 with open(quizID + ".csv", "w") as file:
 	for quiz in quizResponses:
 		file.write("{0},{1},{2},{3}\n".format(quiz[0], quiz[1], quiz[2], quiz[3]))
 
 print("Finished processing {0} quizzes".format(len(quizResponses)))
+print("Saved to file " + quizID + ".csv")
